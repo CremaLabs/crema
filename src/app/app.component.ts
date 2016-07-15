d4e97383e57e6cf35b4aa08fecc39a600e03e242
andrew brown <aebrownz11@gmail.com>
2016-07-14 17:06:13 -0700

    Created login component, template and model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [LoginComponent]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'crema';
 }
--- src/system-config.ts
+++ src/system-config.ts
@@ -34,6 +34,8 @@ const barrels: string[] = [
   // App specific barrels.
   'app',
   'app/shared',
+  'app/login/login',
+  'app/login',
   /** @cli-barrel */
 ];
 
