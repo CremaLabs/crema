52251b1fc56578328157832bc0b62b9387d63437
ssuksawat@worcester.edu <ssuksawat21@gmail.com>
2016-07-20 12:57:34 -0700

    set up page navigation

--- app/app.ts
+++ app/app.ts
@@ -1,17 +1,24 @@
 import {Component} from '@angular/core';
-import {Platform, ionicBootstrap} from 'ionic-angular';
+import {Platform, MenuController, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
-import {SignupPage} from './pages/signup/signup';
+
+import {SigninPage} from './pages/signin/signin';
+
+import {AuthService} from './providers/auth/auth.service';
 
 
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+  template: '<ion-nav [root]="rootPage"></ion-nav>',
+  providers: [ AuthService ]
 })
 export class MyApp {
-  rootPage: any = SignupPage;
+  rootPage:any = SigninPage;
 
-  constructor(platform: Platform) {
-    platform.ready().then(() => {
+  constructor(
+    private platform: Platform,
+    private menu: MenuController
+  ) {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
